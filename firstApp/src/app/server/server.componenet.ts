8de1279dec6752977043f0f72d1b5e33b581997b
Andrew Crawford <andrew.crawford.za@gmail.com>
2018-02-17 09:24:27 +0200

    Property binding

--- firstApp/src/app/server/server.componenet.ts
+++ firstApp/src/app/server/server.componenet.ts
@@ -2,13 +2,22 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  templateUrl : './server.component.html'
+  templateUrl : './server.component.html',
+  styleUrls: ['./server.component.css']
 })
 export class ServerComponent {
   serverId: number = 10;
   serverStatus: string = 'offline';
+  allowNewServer: boolean = false;
 
   getServerStatus() {
     return this.serverStatus;
+  }
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+      console.log(this.allowNewServer);
+    }, 3000);
   }
 }
