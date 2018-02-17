6a7337ff32b58cb9d4470b26aeceeb1489d020f0
Andrew Crawford <andrew.crawford.za@gmail.com>
2018-02-17 09:35:27 +0200

    Add click event (click)

--- firstApp/src/app/server/server.componenet.ts
+++ firstApp/src/app/server/server.componenet.ts
@@ -9,6 +9,7 @@ export class ServerComponent {
   serverId: number = 10;
   serverStatus: string = 'offline';
   allowNewServer: boolean = false;
+  serverCreationStatus = 'No server was created';
 
   getServerStatus() {
     return this.serverStatus;
@@ -19,5 +20,13 @@ export class ServerComponent {
       this.allowNewServer = true;
       console.log(this.allowNewServer);
     }, 3000);
+  }
+
+  // ngOnInit() {
+  //   console.log('ngOnInit');
+  // }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created';
   }
 }
